525fcddddc41492ccca21760b9e40208b36e7aad
lukavuk1 <luka.vuk1@skole.hr>
2016-03-08 18:25:13 +0100

    Procistili smo php.

--- index.php
+++ index.php
@@ -12,6 +12,31 @@
 
 <h1>Forme</h1>
 
+<p>
+
+<form>
+    Ime:<br>
+    <input type="text" name="ime"><br>
+    Prezime:<br>
+    <input type="text" name="prezime"><br>
+    <input type="submit" value="Pošalji">
+    </form>
+<?php echo("Vaše ime je:" . htmlspecialchars($_GET['ime']) . "<br>");
+
+    echo("Vaše prezime je:" . $_GET['prezime']);
+?>
+
+
+
+
+
+
+
+
+</p>
+
+
+
 
 
 </body>
