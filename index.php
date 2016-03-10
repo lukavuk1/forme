f0e8684377a713f508ca91dc1c21ddb03c18fa7b
lukavuk1 <luka.vuk1@skole.hr>
2016-03-10 13:55:13 +0100

    dodali smo forme.css :D

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+    <link rel="stylesheet" type="text/css" href="forme.css">
     <title>Forme</title>
     <meta charset="UTF-8">
 </head>
@@ -14,16 +14,54 @@
 
 <p>
 
-<form>
+<form method="post">
     Ime:<br>
     <input type="text" name="ime"><br>
     Prezime:<br>
     <input type="text" name="prezime"><br>
+    Spol:<br>
+    <input type="radio" name="spol" value="M"> Muško<br>
+    <input type="radio" name="spol" value="Ž"> Žensko<br>
+    <input type="radio" name="spol" value="Ostalo"> Ostalo <br>
+
     <input type="submit" value="Pošalji">
     </form>
-<?php echo("Vaše ime je:" . htmlspecialchars($_GET['ime']) . "<br>");
+<?php
+    if($_POST['spol'] == 'M')
+    {
+        echo("Vaše ime je:" . "<p class='musko'>" . htmlspecialchars($_POST['ime']) . "</p></br>");
+    }
+    elseif($_POST['spol'] == 'Ž')
+    {
+        echo("Vaše ime je:". "<p class='zensko'>" . htmlspecialchars($_POST['ime']) . "</p></br>");
+    }
+    else
+    {
+        echo("Vaše ime je:". "<p class='ostalo'>" . htmlspecialchars($_POST['ime']) . "</p></br>");
+    }
+    echo("Vaše prezime je:" .htmlspecialchars( $_POST['prezime']) . "<br>");
 
-    echo("Vaše prezime je:" . $_GET['prezime']);
+
+    echo("Vaš spol:" .htmlspecialchars( $_POST['spol'])."<br><br><br>");
+
+
+
+
+if($_POST['spol'] == 'M')
+{
+    echo("Vi ste pravi muškarac");
+
+}
+elseif($_POST['spol'] =="Ž")
+{
+    echo("Vi ste prava žena");
+}
+elseif($_POST['spol']=="Ostalo")
+{
+    echo("A šta ste onda?");
+}
+
+
 ?>
 
 
